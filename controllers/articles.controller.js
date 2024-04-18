28e0205055145a5252d3da700e5cdcccb3fb8cc2
Marcus Young <marcus@graspit.co.uk>
2024-04-18 14:18:13 +0100

    refactor of ticket 11 to check for invalid query key

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -14,15 +14,15 @@ function getArticle(req, res, next) {
 
 function getArticles(req, res, next) {
 
-    const queryKeys = Object.keys(req.query) 
+    const queryKeys = Object.keys(req.query)
     const validQueryKeys = ['topic']
 
     if (!queryKeys.every((key) => validQueryKeys.includes(key))) {
-        next({custom_error: {status: 400, msg: "Invalid query key(s)"}})
+        next({ custom_error: { status: 400, msg: "Invalid query key(s)" } })
     }
 
     const { topic } = req.query
-  
+
     selectArticles(topic)
         .then((articles) => {
             res.status(200).send({ articles: articles })
